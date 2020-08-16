6f20292da21763543202e0d6371e02c70c3b2a16
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:19:38 -0500

    Create Numbers.cpp

---
+++ Numbers.cpp
@@ -0,0 +1,21 @@
+int number1 = 10;
+
+cout << number1; // 10
+
+///
+
+double number2 = 1.11
+
+cout << number2; // 1.11
+
+///
+
+char letter = "M";
+
+cout << letter; // M
+
+///
+
+bool hello = true;
+
+cout << hello; // true
