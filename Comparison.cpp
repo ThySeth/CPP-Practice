f820df6a0e5fedfca1341654bad1a4b6025f8cae
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:54:15 -0500

    Create Comparison.cpp

---
+++ Comparison.cpp
@@ -0,0 +1,15 @@
+int a = 10;
+
+int b = 27;
+
+cout << (a == b); // false
+
+cout << (a != b); // true
+
+cout << (a < b); // true
+
+cout << (b <= a); // false
+
+cout << (a > b); // false
+
+cout << (b >= a); // true
