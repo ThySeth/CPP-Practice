7b79356b06bc158deaa160bf86e08e9d99353841
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 05:41:05 -0500

    Create Arrays.cpp

---
+++ Arrays.cpp
@@ -0,0 +1,9 @@
+// Array size clarification (numArray[2]) in the variable is optional, of course
+
+int numArray[2] = [ 12, 24, 36];
+
+cout << numArray[0]; // 12
+
+string strArray[] = [ "'ello", "Hello", "World"];
+
+cout << strArray[0] << " " << strArray[2]; // 'ello World
