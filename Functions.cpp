d8beb11d6060fa4558d3fdb71889e71a7d5da787
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 05:51:02 -0500

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,32 @@
+void Add(int x, int y) {
+  if(!x) {
+    cout << "There is no x value, nerd.";
+  } else if(!y) {
+    cout << "There is no y value, nerd.";
+  }
+  ///
+  cout << x + y
+}
+
+Add(10, 10);
+
+///
+
+void Subtract(int a, int b) {
+  if(!a) {
+    cout << "Gimmie one value.";
+  } else if(!b) {
+    cout << "Gimmie a second value.";
+  }
+  ///
+  cout << a - b;
+}
+
+int c;
+int d;
+
+cin >> c;
+
+cin >> d;
+
+Subtract(c, d);
