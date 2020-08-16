fa9fb23c608598ddf33a0398e44fc7b8707426d5
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 05:29:49 -0500

    Create SwitchStatements.cpp

---
+++ SwitchStatements.cpp
@@ -0,0 +1,17 @@
+int number;
+
+cin >> number;
+
+switch(number) {
+  case 5:
+    cout << "#5. Nice.";
+  break;
+  case 10:
+    cout << "#10. What a legend.";
+  break;
+  case 50:
+    cout << "#50. Ayy.";
+  break;
+  default:
+    cout << number << "... A most curious algorithm...";
+}
