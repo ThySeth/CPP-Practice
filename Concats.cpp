a4f66daf0c78cfb887762aa42e7e745f221f07e4
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:23:24 -0500

    Create Concats.cpp

---
+++ Concats.cpp
@@ -0,0 +1,9 @@
+int minutes = 5;
+
+string food = "Popcorn";
+
+cout << food << " should always be microwaved for " << minutes << " minutes.";
+
+// Popcorn should always be microwaved for 5 minutes.
+
+
