d2b0086284c72cc85ea34035e194ef16e6890b55
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:51:00 -0500

    Create Assignments.cpp

---
+++ Assignments.cpp
@@ -0,0 +1,21 @@
+int a = 10;
+
+a += 10; // 20 (Same as a = a + 10)
+
+///
+
+int b = 5;
+
+b -= 5; // 0 (Same as b = b - 5) 
+
+///
+
+int c = 2;
+
+c *= 2; // 4 (Same as c = c * 2)
+
+///
+
+int d = 10;
+
+d /= 5; // 2 (Same as d = c / 2)
