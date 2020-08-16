6291f6b79517635645f52a82bbfdd6d0e868e9b4
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:38:42 -0500

    Update Inputs.cpp

--- Inputs.cpp
+++ Inputs.cpp
@@ -13,3 +13,21 @@ cin >> b; // 92
 cout << "Ah, hello " << a << ". I can't believe you're " << b << " years old";
 
 // Ah, hello Nick. I can't believe you're 92 years old
+
+///
+
+float num1;
+
+float num2;
+
+cout << "Tell me a number";
+
+cin >> num1;
+
+cout << "Alright. Tell me one more number";
+
+cin >> num2;
+
+cout << num1 << " + " << num2 << " = " << num1 + num2;
+
+// Tell me a number ... Alright. Tell me one more number ... num1 + num2 = <Number>
