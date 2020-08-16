d4df2ffa2e771ad73edd0ed401e8049a54dcbf8c
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-16 04:32:45 -0500

    Create Inputs.cpp

---
+++ Inputs.cpp
@@ -0,0 +1,15 @@
+string a;
+
+int b;
+
+cout << "Tell me your name";
+
+cin >> a; // Nick
+
+cout << "How old are you";
+
+cin >> b; // 92
+
+cout << "Ah, hello " << a << ". I can't believe you're " << b << " years old";
+
+// Ah, hello Nick. I can't believe you're 92 years old
