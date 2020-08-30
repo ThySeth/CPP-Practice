0eedc794f7f3e722677ef9eb94c4d1402695054b
Seth <58237855+SethFromStateFarm@users.noreply.github.com>
2020-08-29 22:09:48 -0500

    ArraySize(); added

--- Arrays.cpp
+++ Arrays.cpp
@@ -4,6 +4,8 @@ int numArray[2] = [ 12, 24, 36];
 
 cout << numArray[0]; // 12
 
+cout << "Array size: " + ArraySize(numArray) + " item(s)";
+
 string strArray[] = [ "'ello", "Hello", "World"];
 
 cout << strArray[0] << " " << strArray[2]; // 'ello World
