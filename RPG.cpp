907995516e42bf00b3e4e302143b5b00e26a0a97
Seth <58237855+ThySeth@users.noreply.github.com>
2020-10-23 21:01:35 -0500

    Converted to C++ from a Java class assignment

---
+++ RPG.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() {
+  
+  std::cout << "You have 15 points. Distribute them across your HEALTH, ATTACK, and LUCK.\n";
+  std::cout << "How many points will you put on HEALTH?\n";
+  
+  int health, attack, luck, points = 15;
+  char yn;
+
+  std::cin >> health;
+  if(health < 1 || health >= 15) {
+   std::cout << "You have to put at least 1 or at most 14 points to this.";
+  }
+  points = points - health;
+  
+  std::cout << "How many points will you put on ATTACK?\n";
+  std::cin >> attack;
+  if(attack < 1 || attack >= 14) {
+    std::cout << "You have to put at least 1 or at most 13 points to this";
+    return 0;
+  }
+  points = points - attack;
+  if(points < 1) {
+    std::cout << "You've used too many points! Try again.";
+    return 0;
+  }
+
+  std::cout << "How many points will you put on LUCK?\n";
+  std::cin >> luck;
+  if(luck < 1 || luck >= 13) {
+    std::cout << "You have to put at least 1 or at most 12 points to this";
+    return 0;
+  }
+  points = points - luck;
+  if(points < 0) {
+    std::cout << "You've used too many points! Try again.";
+    return 0;
+  }
+  
+  std::cout << "Health: " << health << "\nAttack: " << attack << "\nLuck: " << luck << "\nRemaining: " << points << "\n[y/n]\n";
+  std::cin >> yn;
+  
+  if(yn == 'y') {
+   std::cout << "Character set!";
+  } else if(yn == 'n') {
+   std::cout << "Canceled setup!";
+  }
+  
+ return 0;
+}
