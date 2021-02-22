29d5a14a28f8d0419762272e0518c46a2b45fce7
Seth <58237855+ThySeth@users.noreply.github.com>
2021-02-21 23:22:09 -0600

    Create Number.cpp

---
+++ sandbox/Number.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int number;
+  
+  cout << "Input number."; 
+  cin >> number;
+  
+  int number2;
+  
+  cout << "Input number2.";
+  cin >> number2;
+  
+  int sum = number + number2;
+  cout << "The sum is: " << sum;
+
+}
